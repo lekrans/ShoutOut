9af8db812befb22e6d694651004559dbd91ea09b
Michael Lekrans <mlekrans@hotmail.com>
2019-07-23 14:39:09 +0200

    a comment

--- ShoutOutTests/ShoutOutTests.swift
+++ ShoutOutTests/ShoutOutTests.swift
@@ -39,6 +39,7 @@ class ShoutOutTests: XCTestCase {
 	}
 	
 	func testPerformanceExample() {
+        // Just a comment
 		// This is an example of a performance test case.
 		self.measure {
 			// Put the code you want to measure the time of here.
